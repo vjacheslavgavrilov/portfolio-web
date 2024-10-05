b14b04bfd6453b972ab791534e88ee782c71ff5d
Vjacheslav Gavrilov <vjacheslavgavrilov@gmail.com>
2024-10-05 22:17:33 +0300

    add animations and interactions

--- js/show-all.js
+++ js/show-all.js
@@ -2,6 +2,10 @@ function showAll() {
   document.querySelectorAll('.bento-div.case').forEach(div => {
     if (div.classList.contains('show-all-hidden')) {
       div.classList.remove('show-all-hidden');
+      div.style.opacity = '0'; // setting the initial transparency
+      setTimeout(() => {
+        div.style.opacity = '1'; // changing the transparency to 1 after the delay
+      }, 100); // delay in ms
     }
   });
   
