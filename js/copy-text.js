83aff92dae48f22877c3a627f9fcc682af628e11
Vjacheslav Gavrilov <vjacheslavgavrilov@gmail.com>
2024-10-06 23:55:44 +0300

    add smooth appearance and copy button

---
+++ js/copy-text.js
@@ -0,0 +1,32 @@
+function showSuccessToast(message) {
+  var toast = document.querySelector('.toast');
+  toast.innerText = message;
+  toast.style.display = 'block';
+  setTimeout(function() {
+      toast.style.display = 'none';
+  }, 1500);
+}
+
+function showErrorToast(message) {
+  var toast = document.querySelector('.toast');
+  toast.innerText = message;
+  toast.style.display = 'block';
+  setTimeout(function() {
+      toast.style.display = 'none';
+  }, 1500);
+}
+
+function copyText() {
+  if (!navigator.clipboard) {
+    showErrorToast('в браузере запрещено копирование');
+    return;
+  }
+
+  var text = document.querySelector('.primary-button').value;
+  navigator.clipboard.writeText(text).then(function() {
+      showSuccessToast('скопировано');
+  }).catch(function(error) {
+      console.error('Ошибка при копировании текста:', error);
+      showErrorToast('не получилось скопировать');
+  });
+}
